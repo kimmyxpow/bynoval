59eae0ae4a69e0c9eaade7330aebefddca6ef21f
Abi Noval Fauzi <noval@asi-asiapacific.com>
2025-06-10 11:23:17 +0700

    🧼 style: refresh navbar styling to feel cleaner and breezier

--- resources/js/pages/hi.svelte
+++ resources/js/pages/hi.svelte
@@ -13,71 +13,179 @@
 </svelte:head>
 
 <div class="overflow-x-hidden bg-black">
-    <header
-        class="fixed left-1/2 z-10 mt-4 flex h-12 max-w-max -translate-x-1/2 items-center gap-1 rounded-xl bg-zinc-800/80 pr-1 pl-4 backdrop-blur-sm"
-    >
-        <a href="/" class="text-zinc-300">Pow</a>
-        <div class="ml-5 h-3/5 border-r border-zinc-700"></div>
-        <nav class="flex">
-            <a class="flex h-10 items-center rounded-lg px-4 text-zinc-300 transition-all hover:bg-zinc-900" href="/home">Home</a>
+    <header class="fixed left-1/2 z-10 mt-4 max-w-max -translate-x-1/2 bg-gradient-to-r from-black via-cyan-500 to-black p-[0.5px]">
+        <nav class="flex h-12 items-center rounded-xl border-cyan-500 bg-zinc-800 px-1">
+            <a class="grid size-10 place-items-center rounded-lg text-zinc-200 hover:bg-zinc-900" href="/home">
+                <Icon icon="solar:home-2-linear" />
+            </a>
+            <div class="h-3/5 border-r border-zinc-700"></div>
             <a class="flex h-10 items-center rounded-lg px-4 text-zinc-300 transition-all hover:bg-zinc-900" href="/home">Projects</a>
-            <a class="flex h-10 items-center rounded-lg px-4 text-zinc-300 transition-all hover:bg-zinc-900" href="/home">Articles</a>
-            <a class="flex h-10 items-center rounded-lg px-4 text-zinc-300 transition-all hover:bg-zinc-900" href="/home">Snippets</a>
-        </nav>
-        <Popover.Root>
-            <Popover.Trigger>
-                {#snippet child({ props })}
-                    <button
-                        {...props}
-                        class="flex h-10 items-center gap-2 rounded-lg px-4 text-zinc-300 transition-all hover:bg-zinc-900 [&>svg]:transition-all data-[state='open']:[&>svg]:-scale-y-100"
+            <Popover.Root>
+                <Popover.Trigger>
+                    {#snippet child({ props })}
+                        <button
+                            {...props}
+                            class="flex h-10 items-center gap-2 rounded-lg px-4 text-zinc-300 transition-all hover:bg-zinc-900 [&>svg]:transition-all data-[state='open']:[&>svg]:-scale-y-100"
+                        >
+                            Notebook
+                            <Icon icon="solar:alt-arrow-down-linear" class="text-xl" />
+                        </button>
+                    {/snippet}
+                </Popover.Trigger>
+                <Popover.Portal>
+                    <Popover.Content
+                        class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-30 w-full max-w-90 rounded-xl bg-zinc-900 p-2"
+                        sideOffset={8}
                     >
-                        Personal
-                        <Icon icon="solar:alt-arrow-down-linear" class="text-xl" />
-                    </button>
-                {/snippet}
-            </Popover.Trigger>
-            <Popover.Portal>
-                <Popover.Content
-                    class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-30 w-full max-w-90 rounded-xl bg-zinc-900 p-2"
-                    sideOffset={8}
-                >
-                    <div class="grid grid-cols-2 gap-2">
-                        <a
-                            href="/about"
-                            class="flex h-12 items-center gap-2 rounded-lg border border-zinc-700 px-3 font-medium text-zinc-400 transition-all select-none hover:text-zinc-200 focus-visible:outline-none"
+                        <div class="grid gap-2">
+                            <a
+                                href="/about"
+                                class="flex items-start gap-4 rounded-lg border border-zinc-700 p-4 font-medium transition-all select-none hover:bg-zinc-800 focus-visible:outline-none"
+                            >
+                                <div class="mt-1 grid size-10 shrink-0 place-items-center rounded-lg bg-amber-600 text-zinc-200">
+                                    <Icon class="size-6" icon="solar:notes-linear" />
+                                </div>
+                                <div>
+                                    <span class="text-zinc-200">Article</span>
+                                    <p class="text-sm text-zinc-400">
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius ea perspiciatis voluptate minus.
+                                    </p>
+                                </div>
+                            </a>
+                            <a
+                                href="/about"
+                                class="flex items-start gap-4 rounded-lg border border-zinc-700 p-4 font-medium transition-all select-none hover:bg-zinc-800 focus-visible:outline-none"
+                            >
+                                <div class="mt-1 grid size-10 shrink-0 place-items-center rounded-lg bg-fuchsia-600 text-zinc-200">
+                                    <Icon class="size-6" icon="solar:paperclip-rounded-linear" />
+                                </div>
+                                <div>
+                                    <span class="text-zinc-200">Snippets</span>
+                                    <p class="text-sm text-zinc-400">
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius ea perspiciatis voluptate minus.
+                                    </p>
+                                </div>
+                            </a>
+                            <a
+                                href="/about"
+                                class="flex items-start gap-4 rounded-lg border border-zinc-700 p-4 font-medium transition-all select-none hover:bg-zinc-800 focus-visible:outline-none"
+                            >
+                                <div class="mt-1 grid size-10 shrink-0 place-items-center rounded-lg bg-emerald-600 text-zinc-200">
+                                    <Icon class="size-6" icon="solar:paw-linear" />
+                                </div>
+                                <div>
+                                    <span class="text-zinc-200">Guest Book</span>
+                                    <p class="text-sm text-zinc-400">
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius ea perspiciatis voluptate minus.
+                                    </p>
+                                </div>
+                            </a>
+                        </div>
+                    </Popover.Content>
+                </Popover.Portal>
+            </Popover.Root>
+            <Popover.Root>
+                <Popover.Trigger>
+                    {#snippet child({ props })}
+                        <button
+                            {...props}
+                            class="flex h-10 items-center gap-2 rounded-lg px-4 text-zinc-300 transition-all hover:bg-zinc-900 [&>svg]:transition-all data-[state='open']:[&>svg]:-scale-y-100"
                         >
-                            <img class="size-6" src="/assets/animated-icons/about.webp" alt="About Icon" />
-                            About
-                        </a>
-                        <a
-                            href="/about"
-                            class="flex h-12 items-center gap-2 rounded-lg border border-zinc-700 px-3 font-medium text-zinc-400 transition-all select-none hover:text-zinc-200 focus-visible:outline-none"
+                            Personal
+                            <Icon icon="solar:alt-arrow-down-linear" class="text-xl" />
+                        </button>
+                    {/snippet}
+                </Popover.Trigger>
+                <Popover.Portal>
+                    <Popover.Content
+                        class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-30 w-full max-w-90 rounded-xl bg-zinc-900 p-2"
+                        sideOffset={8}
+                    >
+                        <div class="grid grid-cols-2 gap-2">
+                            <a
+                                href="/about"
+                                class="flex h-12 items-center gap-2 rounded-lg border border-zinc-700 px-3 font-medium text-zinc-400 transition-all select-none hover:text-zinc-200 focus-visible:outline-none"
+                            >
+                                <img class="size-6" src="/assets/animated-icons/about.webp" alt="About Icon" />
+                                About
+                            </a>
+                            <a
+                                href="/about"
+                                class="flex h-12 items-center gap-2 rounded-lg border border-zinc-700 px-3 font-medium text-zinc-400 transition-all select-none hover:text-zinc-200 focus-visible:outline-none"
+                            >
+                                <img class="size-6" src="/assets/animated-icons/goals.webp" alt="Goals Icon" />
+                                Goals
+                            </a>
+                            <a
+                                href="/about"
+                                class="flex h-12 items-center gap-2 rounded-lg border border-zinc-700 px-3 font-medium text-zinc-400 transition-all select-none hover:text-zinc-200 focus-visible:outline-none"
+                            >
+                                <img class="size-6" src="/assets/animated-icons/interest.webp" alt="Interest Icon" />
+                                Interest
+                            </a>
+                            <a
+                                href="/about"
+                                class="flex h-12 items-center gap-2 rounded-lg border border-zinc-700 px-3 font-medium text-zinc-400 transition-all select-none hover:text-zinc-200 focus-visible:outline-none"
+                            >
+                                <img class="size-6" src="/assets/animated-icons/gallery.webp" alt="Gallery Icon" />
+                                Gallery
+                            </a>
+                        </div>
+                    </Popover.Content>
+                </Popover.Portal>
+            </Popover.Root>
+            <Popover.Root>
+                <Popover.Trigger>
+                    {#snippet child({ props })}
+                        <button
+                            {...props}
+                            class="flex h-10 items-center gap-2 rounded-lg px-4 text-zinc-300 transition-all hover:bg-zinc-900 [&>svg]:transition-all data-[state='open']:[&>svg]:-scale-y-100"
                         >
-                            <img class="size-6" src="/assets/animated-icons/goals.webp" alt="Goals Icon" />
-                            Goals
-                        </a>
-                        <a
-                            href="/about"
-                            class="flex h-12 items-center gap-2 rounded-lg border border-zinc-700 px-3 font-medium text-zinc-400 transition-all select-none hover:text-zinc-200 focus-visible:outline-none"
-                        >
-                            <img class="size-6" src="/assets/animated-icons/interest.webp" alt="Interest Icon" />
-                            Interest
-                        </a>
-                        <a
-                            href="/about"
-                            class="flex h-12 items-center gap-2 rounded-lg border border-zinc-700 px-3 font-medium text-zinc-400 transition-all select-none hover:text-zinc-200 focus-visible:outline-none"
-                        >
-                            <img class="size-6" src="/assets/animated-icons/gallery.webp" alt="Gallery Icon" />
-                            Gallery
-                        </a>
-                    </div>
-                </Popover.Content>
-            </Popover.Portal>
-        </Popover.Root>
-        <div class="h-3/5 border-r border-zinc-700"></div>
-        <button class="grid size-10 place-items-center rounded-lg text-zinc-200 hover:bg-zinc-900" onclick={() => alert('not yet implemented :p')}>
-            <Icon icon="solar:sun-linear" />
-        </button>
+                            Extras
+                            <Icon icon="solar:alt-arrow-down-linear" class="text-xl" />
+                        </button>
+                    {/snippet}
+                </Popover.Trigger>
+                <Popover.Portal>
+                    <Popover.Content
+                        class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-30 w-full max-w-90 rounded-xl bg-zinc-900 p-2"
+                        sideOffset={8}
+                    >
+                        <div class="grid gap-2">
+                            <a
+                                href="/about"
+                                class="flex h-12 items-center gap-2 rounded-lg border border-zinc-700 px-3 font-medium text-zinc-400 transition-all select-none hover:bg-zinc-800 focus-visible:outline-none"
+                            >
+                                <Icon icon="solar:call-chat-rounded-linear" class="size-6" />
+                                Mentoring
+                            </a>
+                            <a
+                                href="/about"
+                                class="flex h-12 items-center gap-2 rounded-lg border border-zinc-700 px-3 font-medium text-zinc-400 transition-all select-none hover:bg-zinc-800 focus-visible:outline-none"
+                            >
+                                <Icon icon="solar:accessibility-linear" class="size-6" />
+                                Attribution
+                            </a>
+                            <a
+                                href="/about"
+                                class="flex h-12 items-center gap-2 rounded-lg border border-zinc-700 px-3 font-medium text-zinc-400 transition-all select-none hover:bg-zinc-800 focus-visible:outline-none"
+                            >
+                                <Icon icon="radix-icons:discord-logo" class="size-6" />
+                                Community
+                            </a>
+                        </div>
+                    </Popover.Content>
+                </Popover.Portal>
+            </Popover.Root>
+            <a class="flex h-10 items-center rounded-lg px-4 text-zinc-300 transition-all hover:bg-zinc-900" href="/home">Contact Me</a>
+            <div class="h-3/5 border-r border-zinc-700"></div>
+            <button
+                class="grid size-10 place-items-center rounded-lg text-zinc-200 hover:bg-zinc-900"
+                onclick={() => alert('not yet implemented :p')}
+            >
+                <Icon icon="solar:sun-linear" />
+            </button>
+        </nav>
     </header>
     <main class="relative flex min-h-screen items-center">
         <div class="inner">

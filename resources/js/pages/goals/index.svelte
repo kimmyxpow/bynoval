eca63c89bf1ee7bbebfb09cffc7be87b764df006
Abi Noval Fauzi <noval@asi-asiapacific.com>
2025-06-23 16:26:47 +0700

    🎯 feat: add goals page to dream and plan and hope

---
+++ resources/js/pages/goals/index.svelte
@@ -0,0 +1,34 @@
+<script module>
+    export { default as layout } from '@/components/layouts/guest-layout.svelte';
+</script>
+
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+    import { useDebounce } from 'runed';
+    import { router } from '@inertiajs/svelte';
+</script>
+
+<main class="pt-26">
+    <div class="inner">
+        <div class="mx-auto max-w-2xl">
+            <div class="flex flex-col items-center justify-center gap-6 text-center">
+                <span class="font-handwriting text-2xl text-zinc-400">An evolving list of “maybe someday.”</span>
+                <h1 class="text-5xl">A lifelong collection of things I’d love to learn, build, feel, and become</h1>
+            </div>
+        </div>
+    </div>
+</main>
+<section class="py-10">
+    <div class="inner">
+        <div class="mx-auto flex max-w-xl flex-col items-center gap-2">
+            <div class="flex w-full items-center justify-between">
+                <span class="text-lg text-zinc-400">Overall Progress</span>
+                <span class="bg-gradient-to-tr from-cyan-500 to-zinc-200 bg-clip-text text-lg font-semibold text-transparent">4/48</span>
+            </div>
+            <div class="h-4 w-full overflow-hidden rounded-full bg-zinc-800">
+                <div class="h-full w-[8%] bg-gradient-to-tr from-cyan-500 to-zinc-200"></div>
+            </div>
+            <span class="text-zinc-400">8% complete</span>
+        </div>
+    </div>
+</section>
